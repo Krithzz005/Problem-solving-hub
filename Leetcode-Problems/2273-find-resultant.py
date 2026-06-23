628c49fd1e6a2940ddb388fce386b518b7a5af15
SSG Krithigayogasri <missmiracle0507@gmail.com>
2026-06-23 21:12:50 +0530

    Add solution for removing anagrams from a list

---
+++ Leetcode-Problems/2273-find-resultant.py
@@ -0,0 +1,8 @@
+from collections import Counter
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        res=[words[0]]
+        for i in range(1,len(words)):
+            if(Counter(words[i])!=Counter(res[-1])):
+                res.append(words[i])
+        return res
