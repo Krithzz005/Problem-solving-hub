dd84ced6664944fe60e9cf7d052392e92760f136
SSG Krithigayogasri <missmiracle0507@gmail.com>
2026-06-30 21:24:12 +0530

    Implement differenceOfSum method in Solution class

---
+++ Leetcode-Problems/2535-Diff-between.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int sum=0,digitSum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            while(nums[i]>0){
+                digitSum+=nums[i]%10;
+                nums[i]=nums[i]/10;
+            }
+        }return Math.abs(sum-digitSum);
+    }
+}
